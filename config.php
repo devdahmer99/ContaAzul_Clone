b8fc54b27b3b15668f2f094d06d18d74b842ce3d
Eduardo Dahmer Correa <eduardodahmer99@gmail.com>
2022-10-31 15:22:56 -0300

    Criando a estrutura inicial do projeto em MVC

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+require 'environment.php';
+
+global $config;
+$config = array();
+if(ENVIRONMENT == 'development') {
+	$config['dbname'] = 'contaAzul';
+	$config['host'] = 'localhost';
+	$config['dbuser'] = 'root';
+	$config['dbpass'] = '152499';
+} else {
+	$config['dbname'] = 'blog';
+	$config['host'] = 'localhost';
+	$config['dbuser'] = 'root';
+	$config['dbpass'] = 'root';
+}
+?>
